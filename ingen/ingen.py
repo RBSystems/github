b596fb6eaf7020ce7fa7d4659b2e6284d9735b97
zjyx147 <zjyx147@gmail.com>
2017-02-27 11:29:19 -0500

    add first version

---
+++ ingen/ingen.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python
+
+'''
+input generator scripts for qe, rmg and vasp
+'''
+
+import os
+import sys
+
+class RmgIn:
+    def __init__(self):
+
+class ConfParser:
+    def __init__(self, filename="in.conf"):
+        self._confs
+
