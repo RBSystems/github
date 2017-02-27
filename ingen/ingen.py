88b2e8ffc70bc4cb263f7a99ab1245e6a6215bdb
zjyx147 <zjyx147@gmail.com>
2017-02-27 16:15:13 -0500

    modification

--- ingen/ingen.py
+++ ingen/ingen.py
@@ -10,7 +10,22 @@ import sys
 class RmgIn:
     def __init__(self):
 
+class QeIn:
+    def __init__(self):
+
+class Cell:
+    def __init__(self):
+        self.nat = None
+        self.ntype = None
+        self.latvec = None
+        self.
+
 class ConfParser:
     def __init__(self, filename="in.conf"):
-        self._confs
+        self._confs = {}
+        self._params = {}
+
+        self._
+
+def read_poscar(filename):
 
