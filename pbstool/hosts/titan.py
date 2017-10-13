fcc9f0c328c4136dff9e585faabf844236d96d4d
Jiayong Zhang <z8j@titan-ext4.ccs.ornl.gov>
2017-10-13 19:35:03 -0400

    update

--- pbstool/hosts/titan.py
+++ pbstool/hosts/titan.py
@@ -9,7 +9,7 @@ def get_pbs_lines(pbs_dict):
     lines_pbs += "#PBS -m ea\n"
     lines_pbs += "#PBS -M zjyx147@foxmail.com\n"
     lines_pbs += "#PBS -j oe\n"
-    lines_pbs += "#PBS -o out.$PBS_JOBID\n"
+    lines_pbs += "#PBS -o oe.$PBS_JOBID\n"
     lines_pbs += "#PBS -l walltime=%(time)s\n"% pbs_dict
     lines_pbs += "#PBS -l nodes=%(nodes)d\n"% pbs_dict
     lines_pbs += "#PBS -N %(name)s\n"% pbs_dict
@@ -18,10 +18,10 @@ def get_pbs_lines(pbs_dict):
     if exename == "RMG":
         lines_pbs += "module swap PrgEnv-pgi PrgEnv-gnu\n"
     elif exename == "QE":
-        lines_pbs += "module swap PrgEnv-pgi PrgEnv-intel\n"
+        lines_pbs += "module swap PrgEnv-pgi PrgEnv-gnu\n"
     lines_pbs += "module list\n"
     lines_pbs += "\n"
-    lines_pbs += "setenv OMP_NUM_THREADS %(threads)d\n"% pbs_dict
+    lines_pbs += "export OMP_NUM_THREADS=%(threads)d\n"% pbs_dict
     lines_pbs += "\n"
     lines_pbs += "\n"
     lines_pbs += "cd %(pwd)s\n"% pbs_dict
@@ -31,7 +31,8 @@ def get_pbs_lines(pbs_dict):
     if exename == "RMG":
         lines_pbs += "aprun -n %(cores)d -N %(ppn_use)d -d %(threads)d %(exepath)s %(exeinput)s > %(exeoutput)s\n"% pbs_dict
     elif exename == 'QE':
-        lines_pbs += "aprun -n %(cores)d -N %(ppn_use)d -d %(threads)d %(exepath)s < %(exeinput)s > %(exeoutput)s\n"% pbs_dict
+        lines_pbs += "aprun -n %(cores)d -N %(ppn_use)d -d %(threads)d -cc numa_node %(exepath)s < %(exeinput)s > %(exeoutput)s\n"% pbs_dict
+    lines_pbs += "wait\n"
     lines_pbs += "\n"
     lines_pbs += "date\n"
 
