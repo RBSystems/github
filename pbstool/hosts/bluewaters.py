6243b87b6e1b0d2f200aba607bb9f594057e2177
Jiayong Zhang <jzhang57@ncsu.edu>
2017-10-17 10:01:46 -0500

    oe.$PBSJOB

--- pbstool/hosts/bluewaters.py
+++ pbstool/hosts/bluewaters.py
@@ -9,7 +9,7 @@ def get_pbs_lines(pbs_dict):
     lines_pbs += "#PBS -m ea\n"
     lines_pbs += "#PBS -M zjyx147@foxmail.com\n"
     lines_pbs += "#PBS -j oe\n"
-    lines_pbs += "#PBS -o out.$PBS_JOBID\n"
+    lines_pbs += "#PBS -o oe.$PBS_JOBID\n"
     lines_pbs += "#PBS -l walltime=%(time)s\n"% pbs_dict
     lines_pbs += "#PBS -l nodes=%(nodes)d:ppn=32:xe\n"% pbs_dict
     lines_pbs += "#PBS -N %(name)s\n"% pbs_dict
@@ -18,20 +18,21 @@ def get_pbs_lines(pbs_dict):
     if exename == "RMG":
         lines_pbs += "module swap PrgEnv-cray PrgEnv-gnu\n"
     elif exename == "QE":
-        lines_pbs += "module swap PrgEnv-cray PrgEnv-intel\n"
+        lines_pbs += "module swap PrgEnv-cray PrgEnv-gnu\n"
     lines_pbs += "module list\n"
     lines_pbs += "\n"
     lines_pbs += "export OMP_NUM_THREADS=%(threads)d\n"% pbs_dict
     lines_pbs += "\n"
-    lines_pbs += "export MPICH_MAX_THREAD_SAFETY=serialized\n"
-    lines_pbs += "export OMP_WAIT_POLICY=passive\n"
-    lines_pbs += "export MPICH_ENV_DISPLAY=1\n"
-    lines_pbs += "export MPICH_ALLREDUCE_NO_SMP=1\n"
-    lines_pbs += "ulimit -a\n"
-    lines_pbs += "export CRAY_CUDA_PROXY=1\n"
-    lines_pbs += "export MPICH_UNEX_BUFFER_SIZE=362914560\n"
-    lines_pbs += "export MPICH_MAX_SHORT_MSG_SIZE=3200\n"
-    lines_pbs += "\n"
+    if exename == "RMG":
+        lines_pbs += "export MPICH_MAX_THREAD_SAFETY=serialized\n"
+        lines_pbs += "export OMP_WAIT_POLICY=passive\n"
+        lines_pbs += "export MPICH_ENV_DISPLAY=1\n"
+        lines_pbs += "export MPICH_ALLREDUCE_NO_SMP=1\n"
+        lines_pbs += "ulimit -a\n"
+        lines_pbs += "export CRAY_CUDA_PROXY=1\n"
+        lines_pbs += "export MPICH_UNEX_BUFFER_SIZE=362914560\n"
+        lines_pbs += "export MPICH_MAX_SHORT_MSG_SIZE=3200\n"
+        lines_pbs += "\n"
     lines_pbs += "cd %(pwd)s\n"% pbs_dict
     lines_pbs += "\n"
     lines_pbs += "date\n"
@@ -42,6 +43,7 @@ def get_pbs_lines(pbs_dict):
         lines_pbs += "aprun -n %(cores)d -N %(ppn_use)d -d %(threads)d -cc numa_node %(exepath)s < %(exeinput)s > %(exeoutput)s\n"% pbs_dict
     elif exename == 'VASP':
         lines_pbs += "aprun -n %(cores)d -N %(ppn_use)d -d %(threads)d -cc numa_node %(exepath)s\n"% pbs_dict
+    lines_pbs += "wait\n"
     lines_pbs += "\n"
     lines_pbs += "date\n"
 
